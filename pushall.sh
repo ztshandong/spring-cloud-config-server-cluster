7814ba97d1e801d82861b9f07c7d3a4ef63cf3b2
zhangtao <ztshandong@gmail.com>
2017-06-16 22:11:32 +0800

    spring cloud config server as eureka client

---
+++ pushall.sh
@@ -0,0 +1,5 @@
+git push osc master
+git push ztosc master
+git push aliyun master
+git push gitlab master
+git push bitbucket master
---
+++ src/main/java/com/zhangtao/ConfigserverApplication.java
@@ -0,0 +1,26 @@
+package com.zhangtao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverApplication extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+	//忽略xml格式
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+	}
+}
