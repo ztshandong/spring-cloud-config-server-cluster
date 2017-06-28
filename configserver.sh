c01b78a76f90b79f929afa4de695737ab4df3315
zhangtao <ztshandong@gmail.com>
2017-06-28 14:01:58 +0800

    better

---
+++ configserver.sh
@@ -0,0 +1,3 @@
+nohup java -jar target/configserver-0.0.1-SNAPSHOT.jar --spring.profiles.active=first&
+nohup java -jar target/configserver-0.0.1-SNAPSHOT.jar --spring.profiles.active=second&
+
--- src/main/java/com/zhangtao/ConfigserverApplication.java
+++ src/main/java/com/zhangtao/ConfigserverApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
+//@EnableEurekaClient
 public class ConfigserverApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
